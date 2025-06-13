cf5b99bdb55c3421e0ba8df4d314e737283906e4
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-13 12:19:48 -0300

    st

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    files: ['**/*.ts'],
+    ignores: ['dist/', 'dev/']
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    rules: {
+      ...prettier.rules,
+    },
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export function drawSomething(ctx: CanvasRenderingContext2D) {
+  ctx.fillStyle = 'white';
+  ctx.font = '24px sans-serif';
+  ctx.fillText('Batukit begins!', 50, 100);
+}
